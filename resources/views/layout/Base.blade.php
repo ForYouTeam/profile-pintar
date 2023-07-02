aa49c03fb3f2b7757097895f6773e80afe8941ab
IPKIN9 <irwandirini@gmail.com>
2023-07-02 09:38:47 +0800

    Setup ui done

---
+++ resources/views/layout/Base.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layout.Head')
+
+<body>
+    <!-- [ Pre-loader ] start -->
+    <div class="loader-bg">
+        <div class="loader-track">
+            <div class="loader-fill"></div>
+        </div>
+    </div>
+    <!-- [ Pre-loader ] End -->
+    <!-- [ navigation menu ] start -->
+    @include('layout.NavHeader')
+    <!-- [ Header ] end -->
+
+    <!-- [ Main Content ] start -->
+    <div class="pcoded-main-container">
+        <div class="pcoded-wrapper container">
+            <div class="pcoded-content">
+                <div class="pcoded-inner-content">
+                    <div class="main-body">
+                        <div class="page-wrapper">
+                            <div class="page-header">
+                                <div class="page-block">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-12">
+                                            @yield('header-content')
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            @yield('main-content')
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- Required Js -->
+        <script src="{{ asset('assets/js/vendor-all.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/ripple.js') }}"></script>
+        <script src="{{ asset('assets/js/pcoded.min.js') }}"></script>
+
+
+    <!-- prism Js -->
+    <script src="{{ asset('assets/js/plugins/prism.js') }}"></script>
+
+    
+
+
+
+    <script src="{{ asset('assets/js/horizontal-menu.js') }}"></script>
+    <script>
+        (function() {
+            if ($('#layout-sidenav').hasClass('sidenav-horizontal') || window.layoutHelpers.isSmallScreen()) {
+                return;
+            }
+            try {
+                window.layoutHelpers._getSetting("Rtl")
+                window.layoutHelpers.setCollapsed(
+                    localStorage.getItem('layoutCollapsed') === 'true',
+                    false
+                );
+            } catch (e) {}
+        })();
+        $(function() {
+            $('#layout-sidenav').each(function() {
+                new SideNav(this, {
+                    orientation: $(this).hasClass('sidenav-horizontal') ? 'horizontal' : 'vertical'
+                });
+            });
+            $('body').on('click', '.layout-sidenav-toggle', function(e) {
+                e.preventDefault();
+                window.layoutHelpers.toggleCollapsed();
+                if (!window.layoutHelpers.isSmallScreen()) {
+                    try {
+                        localStorage.setItem('layoutCollapsed', String(window.layoutHelpers.isCollapsed()));
+                    } catch (e) {}
+                }
+            });
+        });
+        $(document).ready(function() {
+            $("#pcoded").pcodedmenu({
+                themelayout: 'horizontal',
+                MenuTrigger: 'hover',
+                SubMenuTrigger: 'hover',
+            });
+        });
+    </script>
+
+    <script src="{{ asset('assets/js/analytics.js') }}"></script>
+    @yield('script')
+</body>
+
+</html>
---
+++ resources/views/pages/Dashboard.blade.php
@@ -0,0 +1,51 @@
+@extends('layout.Base')
+@section('header-content')
+<div class="page-header-title">
+  <h5 class="m-b-10">Dashboard</h5>
+</div>
+<ul class="breadcrumb">
+  <li class="breadcrumb-item"><a href="index.html"><i class="feather icon-home"></i></a></li>
+</ul>
+@endsection
+@section('main-content')
+{{-- <div class="card">
+  <div class="card-header">
+      <h5>Basic Table</h5>
+      <span class="d-block m-t-5">use class <code>table</code> inside table element</span>
+  </div>
+  <div class="card-body table-border-style">
+      <div class="table-responsive">
+          <table class="table">
+              <thead>
+                  <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>1</td>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                  </tr>
+                  <tr>
+                      <td>2</td>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                  </tr>
+                  <tr>
+                      <td>3</td>
+                      <td>Larry</td>
+                      <td>the Bird</td>
+                      <td>@twitter</td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  </div>
+</div> --}}
+@endsection
